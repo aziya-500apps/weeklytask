6286b6a1d2f9f0d1e155ff3b2cce6b54c2d44a10
aziya-500apps <aziya.shaik@500apps.com>
2022-04-07 11:20:12 +0530

    commit

--- assignment06-04-2022/src/router/index.js
+++ assignment06-04-2022/src/router/index.js
@@ -6,6 +6,7 @@ import TenTh from "../components/10thques.vue"
 import EighTh from "../components/8thques.vue"
 import FifTh from "../components/5thques.vue"
 import NineTh from "../components/9thques.vue"
+import FirstTried from "../components/firstquestried.vue"
 
 
 Vue.use(Router)
@@ -17,6 +18,7 @@ const routes=[
     {path:"/five",name:"FifTh",component:FifTh},
     {path:"/three",name:"ThirD",component:ThirD},
     {path:"/nine",name:"NineTh",component:NineTh},
+    {path:"/firsttried",name:"FirstTried ",component:FirstTried },
 
 ]
 let router =new Router({routes})
