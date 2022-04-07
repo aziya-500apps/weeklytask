ffcf751af0e49bfcf56145ff8cab661609b7eb35
aziya-500apps <aziya.shaik@500apps.com>
2022-04-07 18:23:05 +0530

    commit

--- assignment06-04-2022/src/components/tried4th.vue
+++ assignment06-04-2022/src/components/tried4th.vue
@@ -5,8 +5,6 @@
   </div>
 </template>
 <script>
-//var axios = require("axios").default;
-
 export default {
   name: "TrieD4",
   data() {
@@ -20,7 +18,8 @@ export default {
       try {
         let response = await fetch("https://api.publicapis.org/entries");
         this.posts = await response.json();
-      } catch (error) {
+      } 
+      catch (error) {
         console.log(error);
       }
     },
@@ -30,3 +29,11 @@ export default {
   },
 };
 </script>
+
+<!--const getData = () => {     // return promise object when function is executed     
+return new Promise((resolve, reject) => {         // Using axios to fetch API URL         
+axios             .get("https://api.publicapis.rg/entries")             .then((response) => {                
+     // resolve when response is received                 resolve(response.data);             })             
+     .catch((error) => {                 // reject when error is occured                 reject(error);            
+      });     }); } // Hit API and fetch data through function var response = getData() response.then((data) => {     
+          console.log("response", data) }).catch((err) => {     console.log(err.toJSON().message) })-->
