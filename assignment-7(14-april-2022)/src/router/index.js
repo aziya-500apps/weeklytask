135453655a419c606b7dd7c0f205341915b39d95
aziya-500apps <aziya.shaik@500apps.com>
2022-04-14 17:59:41 +0530

    commit

---
+++ assignment-7(14-april-2022)/src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue"
+import Router from "vue-router"
+import First from "../components/2ndques"
+
+Vue.use(Router)
+const routes=[
+    
+   
+   
+   {path:"/1",name:"Firstnew1",component:First},
+
+]
+let router =new Router({routes})
+export default router
